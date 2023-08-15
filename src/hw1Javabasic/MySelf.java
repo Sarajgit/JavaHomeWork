c8fbff225945298fbab04caccd165b307987e229
Sarajgit <humtumthatsit@gmail.com>
2023-08-15 12:38:07 -0400

    javahomework

---
+++ src/hw1Javabasic/MySelf.java
@@ -0,0 +1,25 @@
+package hw1Javabasic;
+
+/*
+ * slash+shift8(*)+enter=multiple line
+ * project name start upper case 
+ * package name start with lower case
+ * class name start with upper case
+ * camel case as like "SarajRonyRasel
+ * alternative  of Camel case is snake case
+ */
+
+public class MySelf {
+
+//Main+enter
+//Main method=	Entry point of java
+//This is part of Editor
+//shift+ctrl+f=organize
+//line number (3-10)are multiple comments,Line number (14-18) are single comments
+
+	public static void main(String[] args) {
+		System.out.println("Sarajul islam");
+		System.out.println("Brooklyn,NY");
+
+	}
+}
---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,25 @@
+package hw2JavaBasics;
+
+//I create package toollbar on top golden color and class is green color
+
+public class MyCar {
+
+	public static void main(String[] args) {
+//line 8-14 I am working on print and println to run as next to and up and down
+
+		System.out.print("My name is \"Sarajul Islam\". ");
+		System.out.print("I love to drive");
+		System.out.println("\nMy car info are below:");
+		System.out.println("\ttoyota highlande" + "and it's hybried");
+		System.out.println("\tCar color is balck");
+		System.out.println("\tIt's a awd car");
+		System.out.println("\tI Love my car");
+
+//line 11 i use \''  for highlight my name
+//line 13 i use \n for next line
+//line 14 i practice string concatenation
+//line 14-17 i practice \t for tab
+
+	}
+
+}
